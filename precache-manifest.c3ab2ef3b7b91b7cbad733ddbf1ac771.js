e45c36019435904e977c439ed8230529398fbf41
nchakrawarty <nchakrawarty@gmail.com>
2020-03-03 12:57:54 +0530

    Updates

---
+++ precache-manifest.c3ab2ef3b7b91b7cbad733ddbf1ac771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585d673f350999b828d30d9324717d2e",
+    "url": "/ReactForm2/index.html"
+  },
+  {
+    "revision": "a4329135b3722f82c31a",
+    "url": "/ReactForm2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "82db4576b70c2262bb08",
+    "url": "/ReactForm2/static/js/2.1f7f4939.chunk.js"
+  },
+  {
+    "revision": "303c586a3b9a802cfe6a1fddd3e10e1d",
+    "url": "/ReactForm2/static/js/2.1f7f4939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4329135b3722f82c31a",
+    "url": "/ReactForm2/static/js/main.4745fbb8.chunk.js"
+  },
+  {
+    "revision": "d1fb64411dcab9330212",
+    "url": "/ReactForm2/static/js/runtime-main.0293189a.js"
+  }
+]);
